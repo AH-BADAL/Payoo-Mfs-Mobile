a7b432b2fe3cc35c56f1c738f3865a816a48c4f4
Akhter Hossen Badal <akterhossenbadal810@gmail.com>
2025-10-29 12:34:00 +0600

    some change

--- amaount.js
+++ amaount.js
@@ -13,5 +13,27 @@ document.getElementById('add-money').addEventListener('click', function (event) 
   const addPincode = document.getElementById('add-pin').value;
   console.log(addPincode);
 
+  if (addPincode === '1234') {
+    console.log('add Money')
+  }
+  else {
+    window.alert('Wrong Number or pin Please try again');
+  }
+  
+  const Balance = document.getElementById('Main-balance').innerText;
+  console.log(Balance);
+
+
+  const addMoneyNumber = parseFloat(addAmaountInput);
+  const blanceNumber = parseFloat(Balance);
+  const Newblance = addMoneyNumber + blanceNumber;
+  console.log(Newblance);
+
+
+  document.getElementById('Main-balance').innerText = Newblance;
+
+
+
+
 
 });
